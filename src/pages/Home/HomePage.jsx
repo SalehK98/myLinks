e3c3da272cda7018ea6197da6b2f6b5442b56e29
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-05 19:37:32 +0300

    finished styling usercard

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import UserCard from "../../components/userCard/UserCard";
 
 export default function HomePage() {
   return (
     <>
+      <UserCard />
+      --------------------------------
       <div>SidePanel</div>
       <div>mainSection</div>
     </>
