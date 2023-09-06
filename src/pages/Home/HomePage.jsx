175539b65e94a4e7027e93e36912acf203ee3451
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-06 19:38:46 +0300

    commit

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import UserCard from "../../components/UserCard/UserCard";
 import CategoryBlock from "../../components/CategoryBlock/CategoryBlock";
 
 export default function HomePage() {
