fbe5d7e5ae504031e2dc29cf057e3d0d47104156
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-07 11:51:16 +0300

    set basic styling for sections

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,13 +1,22 @@
-import React from "react";
 import CategoryBlock from "../../components/CategoryBlock/CategoryBlock";
+import styles from "../../styles/HomePage.module.css";
 
 export default function HomePage() {
   return (
     <>
-      <div>SidePanel</div>
-      <CategoryBlock />
-      <CategoryBlock />
-      <CategoryBlock />
+      <div className={styles.pageContainer}>
+        <div className={styles.sidePanel}>
+          {/* Content for the side panel */}
+          SidePanel
+          <CategoryBlock />
+        </div>
+        <div className={styles.mainContent}>
+          {/* Content for the main content area */}
+          <CategoryBlock />
+          <CategoryBlock />
+          <CategoryBlock />
+        </div>
+      </div>
     </>
   );
 }
