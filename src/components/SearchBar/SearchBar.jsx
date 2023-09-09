1fcc6c68fcbefb6bf5955531480095da54f3f05e
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-09 13:07:21 +0300

    almost all ui is ready

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,28 @@
 import styles from "../../styles/SearchBar.module.css";
+import { useState, useEffect } from "react";
+import { BsSearch } from "react-icons/bs";
 
 export default function SearchBar() {
+  const [scrollEffect, setScrollEffect] = useState(
+    styles.SearchBarWrapperBeforeScroll
+  );
+
+  const onScroll = () => {
+    if (window.scrollY >= 1) {
+      console.log("scrolled");
+      setScrollEffect(styles.SearchBarWrapperAfterScroll);
+    } else {
+      console.log("not scrolled");
+      setScrollEffect(styles.SearchBarWrapperBeforeScroll);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  });
+
   return (
-    <div className={styles.SearchBarWrapper}>
+    <div className={scrollEffect}>
       <div className={styles.searchContainer}>
         {/* <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" /> */}
         {/* <i class="fa-solid fa-magnifying-glass" style="color: #85878a;"></i> */}
@@ -11,10 +31,11 @@ export default function SearchBar() {
           placeholder={" Search by name, url or category"}
           className={styles.searchInput}
         />
-        <button className={styles.searchButton}>
-          {/* Font Awesome search icon */}
-          <i className="fa fa-search"></i>
-        </button>
+        {/* <button className={styles.searchButton}> */}
+        {/* Font Awesome search icon */}
+        {/* <i className="fa fa-search"></i> */}
+        <BsSearch />
+        {/* </button> */}
       </div>
     </div>
   );
