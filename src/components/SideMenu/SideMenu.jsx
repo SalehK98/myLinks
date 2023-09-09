bfd88605364e7c247546e4231362e0a904234f9c
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-09 18:56:26 +0300

    fix deploy issue2

--- src/components/SideMenu/SideMenu.jsx
+++ src/components/SideMenu/SideMenu.jsx
@@ -1,6 +1,6 @@
 import categories from "../../data/links.json";
 import styles from "../../styles/SideMenu.module.css";
-import UserCard from "../UserCard/UserCard";
+import UserCard from "../UserCard/UserCard.jsx";
 
 console.log(Object.values(categories));
 
