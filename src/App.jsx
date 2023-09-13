8b14d405e640e18abc8973c0ddf09b22518a7570
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-13 20:01:57 +0300

    created generic login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import CategoryBlock from "./components/CategoryBlock/CategoryBlock";
 import HomePage from "./pages/Home/HomePage";
+import LoginPage from "./pages/Login/LoginPage";
 
 function App() {
   return (
     <>
-      <HomePage />
+      {/* <HomePage /> */}
+      <LoginPage />
     </>
   );
 }
---
+++ src/pages/Login/LoginPage.jsx
@@ -0,0 +1,28 @@
+import styles from "../../styles/LoginPage.module.css";
+
+export default function LoginPage() {
+  return (
+    //     <div className={styles.pageContainer}>
+    //       <h1>
+    //         <span>MyLinks</span> <i>Web</i>
+    //       </h1>
+    //       <div className={styles.}></div>
+    //     </div>
+    <div className={styles.loginContainer}>
+      <h1 className={styles.title}>Login</h1>
+      <div className={styles.welcomeBox}>
+        <p className={styles.welcomeText}>Welcome!</p>
+        <p className={styles.mustLoginText}>You must log in to continue.</p>
+        <button className={styles.googleLoginButton}>
+          <span className={styles.googleIcon}>G</span> Log in with Google
+        </button>
+      </div>
+      <div className={styles.termsContainer}>
+        <p className={styles.termsText}>
+          By logging in, you agree to our <a href="#">Terms of Service</a> and{" "}
+          <a href="#">Privacy Policy</a>.
+        </p>
+      </div>
+    </div>
+  );
+}
