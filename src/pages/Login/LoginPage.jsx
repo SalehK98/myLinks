4bea2779c1ee4c25d6e949970971a6aafba7cec3
Saleh Kalouti <salehkalouti@gmail.com>
2023-09-17 17:43:20 +0300

    finished login page and styling

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,31 +1,22 @@
 import styles from "../../styles/LoginPage.module.css";
 import linkIcon from "../../assets/icons/icons8-link-96.webp";
+import LoginCard from "../../components/LoginCard/LoginCard";
 
 export default function LoginPage() {
   return (
-    //     <div className={styles.pageContainer}>
-    //       <h1>
-    //         <span>MyLinks</span> <i>Web</i>
-    //       </h1>
-    //       <div className={styles.}></div>
-    //     </div>
     <div className={styles.loginContainer}>
-      <h1 className={styles.title}>
-        <img src={linkIcon} style={{ width: "4rem" }} /> &nbsp;<b>MyLinks</b>{" "}
-        &nbsp;Web
-      </h1>
-      <div className={styles.welcomeBox}>
-        <p className={styles.welcomeText}>Welcome!</p>
-        <p className={styles.mustLoginText}>You must log in to continue.</p>
-        <button className={styles.googleLoginButton}>
-          <span className={styles.googleIcon}>G</span> Log in with Google
-        </button>
-        <div className={styles.termsContainer}>
-          <p className={styles.termsText}>
-            By logging in, you agree to our <a href="#">Terms of Service</a> and{" "}
-            <a href="#">Privacy Policy</a>.
-          </p>
-        </div>
+      <div className={styles.title}>
+        <img src={linkIcon} />
+        <h1>
+          &nbsp;<b>MyLinks</b>
+        </h1>
+        &nbsp;&nbsp;&nbsp;
+        <span>
+          <i>Web</i>
+        </span>
+      </div>
+      <div className={styles.cardWrapper}>
+        <LoginCard />
       </div>
     </div>
   );
